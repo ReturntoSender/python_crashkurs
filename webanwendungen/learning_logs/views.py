24d255773fb55494195a27ac36a80b9896534337
TerraXF@gmail.com <Sonnikita2003>
2021-11-15 15:41:16 +0100

    add forms

--- webanwendungen/learning_logs/views.py
+++ webanwendungen/learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -23,3 +24,18 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'entries': entries, 'topic': topic, 'title': title}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        # Keine Daten übermittelt; es wird ein leeres Formular erstellt.
+        form = TopicForm()
+    else:
+        # POST-Daten übermittelt; Daten werden verarbeitet.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    # Zeigt ein leeres oder ein als ungültig erkanntes Formular an.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
