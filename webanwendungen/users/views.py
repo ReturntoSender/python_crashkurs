f4614b34fa31d001bc5ad74a85c834b6a47410d1
TerraXF@gmail.com <Sonnikita2003>
2021-11-16 15:29:47 +0100

    add register site

--- webanwendungen/users/views.py
+++ webanwendungen/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.signals import user_logged_in
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """Register a new user."""
+    if request.method != 'POST':
+        # Zeigt das leere Registrierungsformular an.
+        form = UserCreationForm()
+    else:
+        # Verarbeitet das ausgefüllte Formular.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Meldet den Benutzer an und leitet ihn zur Startseite
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # Zeigt ein leeres oder ein als ungültig erkanntes Formular an.
+    title = 'Register'
+    context = {'form': form, 'title': title}
+    return render(request, 'registration/register.html', context)
