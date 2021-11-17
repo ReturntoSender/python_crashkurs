73275699f45a413f59c83e01ba5cf0a8595a5491
ReturntoSender <TerraXF@gmail.com>
2021-11-17 19:18:27 +0100

    added bootstrap4

--- webanwendungen/learning_log/settings.py
+++ webanwendungen/learning_log/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    # Drittanbieter Apps
+    'bootstrap4',
+
     # Django-Standard-Apps
     'django.contrib.admin',
     'django.contrib.auth',
