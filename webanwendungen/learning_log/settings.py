c574c486e8b93353a59ac75a487bab4d52d8352b
ReturntoSender <TerraXF@gmail.com>
2021-11-17 23:21:31 +0100

    Ready for deployment on Heroku.

--- webanwendungen/learning_log/settings.py
+++ webanwendungen/learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Eigene Einstellungen
 LOGIN_URL = 'users:login'
+
+# Heroku-Einstellungen
+django_heroku.settings(locals())
